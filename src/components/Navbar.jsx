07a787b5e2698bec97d02f4442b6feda37efb410
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-02 23:08:05 +0700

    update: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="container">
-        <Link to="/" className="navbar-brand">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
           Deadline Warrior Blog
         </Link>
+        <div className="navbar-links">
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
+          <Link to="/posts" className="navbar-link">
+            Posts
+          </Link>
+        </div>
       </div>
     </nav>
   );
